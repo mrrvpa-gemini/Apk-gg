3b31c4882321ee1fad46969d02244b213c799297
root <root@localhost.localdomain>
2026-04-08 17:01:36 +0000

    ULTIMATE CLEANUP: Reset Gradle Structure kasta Maestro

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,16 +8,12 @@ android {
     namespace = "com.nullx.tr4s"
     compileSdk = 34
 
-    sourceSets {
-        getByName("main").java.srcDirs("src/main/kotlin")
-    }
-
     defaultConfig {
         applicationId = "com.nullx.tr4s"
         minSdk = 21
         targetSdk = 34
-        versionCode = flutter.versionCode ?: 1
-        versionName = flutter.versionName ?: "1.0.0"
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     compileOptions {
@@ -27,7 +23,6 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-        freeCompilerArgs += "-Xskip-metadata-version-check"
     }
 }
 
